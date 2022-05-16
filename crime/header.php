533f658873b9c4ba2c5e10043871b65cd740e34d
Mibek Shrestha <79530597+Mibek-Shrestha@users.noreply.github.com>
2022-05-16 11:59:22 +0545

    Add files via upload

---
+++ crime/header.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style/header.css">
+   
+    <title>header</title>
+</head>
+
+<body>
+
+    <div>
+        <nav>
+            <a href="#"> 
+                <img id="logo" src="images/logo.png" alt="logo" style="height:75px;">
+            </a>     
+            <ul class="nav-links">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <li> <a href="#">Home</a></li>
+                <li> <a href="#">News</a></li>
+                <li> <a href="#">Login</a></li>
+                <li> <a href="#">Register</a></li>
+             
+     
+            </ul>
+            <div class="burger">
+                <div class="line1"></div>
+                <div class="line2"></div>
+                <div class="line3"></div>
+            </div>
+        </nav>
+       
+    </div>
+    <script src="./js/header.js"></script>
+</body>
+</html>
